31e197281ad3a0ec035c0a87612809394afb0c2e
xAminxxx <maknimedamin8@gmail.com>
2026-05-16 18:37:31 +0100

    fix: simplify metro config for windows compatibility

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,19 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Ensure we don't accidentally overwrite important defaults
-const {
-  resolver: { sourceExtensions, assetExtensions },
-} = config;
-
-config.resolver.sourceExtensions = [
-  ...sourceExtensions,
-  'ts', 'tsx', 'js', 'jsx', 'json',
-];
-
-config.resolver.assetExtensions = [
-  ...assetExtensions,
-  'ttf', 'otf', 'png', 'jpg', 'jpeg', 'svg'
-];
+// Ensure fonts and images are included in the asset extensions
+config.resolver.assetExtensions.push('ttf', 'otf', 'png', 'jpg', 'jpeg', 'svg');
 
 module.exports = config;
